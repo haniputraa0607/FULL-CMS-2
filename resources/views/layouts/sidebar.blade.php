972158af1588b4b88be6f57a9721c1771f9bc109
raka yulian <raka@technopartner.co.id>
2021-10-19 08:57:57 +0700

    update sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -475,7 +475,7 @@
 		@if(MyHelper::hasAccess([338,339,340,341], $grantedFeature))
 		<li class="nav-item {{($menu_active == 'project') ? 'active open' : ''}}">
 			<a href="javascript:;" class="nav-link nav-toggle">
-				<i class="fa fa-project-diagram"></i>
+				<i class="fa fa-file"></i>
 				<span class="title">Partners</span>
 				<span class="arrow {{($menu_active == 'partners') ? 'open' : ''}}"></span>
 			</a>
