db74fce90757add08559492830e2a4810b4c0708
tpid-hanif <hanif@technopartner.co.id>
2021-12-14 16:24:02 +0700

    fixed bug partner view

--- Modules/BusinessDevelopment/Resources/views/locations/steps/calculation.blade.php
+++ Modules/BusinessDevelopment/Resources/views/locations/steps/calculation.blade.php
@@ -7,10 +7,67 @@
                 $follow_up = $step['follow_up'];
                 $note = $step['note'];
                 $file = $step['attachment'];
+                $result['total_payment'] = number_format($result['total_payment'] ,0, '.' , '.' );
             }
         }
     }
 ?>
+
+<script>
+    $(document).ready(function () {
+        $("input[data-type='currency']").on({
+            keyup: function() {
+              formatCurrency($(this));
+            },
+            blur: function() { 
+              formatCurrency($(this), "blur");
+            }
+        });
+        function formatNumber(n) {
+          // format number 1000000 to 1,234,567
+          return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        }
+        function formatCurrency(input, blur) {
+          // appends $ to value, validates decimal side
+          // and puts cursor back in right position.
+          // get input value
+          var input_val = input.val();
+          // don't validate empty input
+          if (input_val === "") { return; }
+          // original length
+          var original_len = input_val.length;
+          // initial caret position 
+          var caret_pos = input.prop("selectionStart");
+          // check for decimal
+          if (input_val.indexOf(".") >= 0) {
+            // get position of first decimal
+            // this prevents multiple decimals from
+            // being entered
+            var decimal_pos = input_val.indexOf(".");
+            // split number by decimal point
+            var left_side = input_val.substring(0, decimal_pos);
+            // add commas to left side of number
+            left_side = formatNumber(left_side);
+            // join number by .
+            input_val = left_side ;
+          } else {
+            // no decimal entered
+            // add commas to number
+            // remove all non-digits
+            input_val = formatNumber(input_val);
+            input_val = input_val;
+            // final formatting
+            
+          }
+          // send updated string to input
+          input.val(input_val);
+          // put caret back in the right position
+          var updated_len = input_val.length;
+          caret_pos = updated_len - original_len + caret_pos;
+          input[0].setSelectionRange(caret_pos, caret_pos);
+        }
+    })
+</script>
 
 <div style="white-space: nowrap;">
     <div class="tab-pane">
@@ -56,7 +113,10 @@
                                     <label for="example-search-input" class="control-label col-md-4">Total Payment <span class="required" aria-required="true">*</span>
                                         <i class="fa fa-question-circle tooltips" data-original-title="Nominal yang akan digunakan untuk perhitungan detail produk invoice" data-container="body"></i></label>
                                     <div class="col-md-5">
-                                        <input class="form-control" type="text" id="total_payment" name="total_payment" placeholder="Enter rental price here" @if ($calcu==true) value="{{ $result['total_payment'] }}" readonly @endif required/>
+                                        <div class="input-group">
+                                            <span class="input-group-addon">Rp</span>
+                                            <input class="form-control" type="text" id="total_payment" name="total_payment" placeholder="Enter rental price here" @if ($calcu==true) value="{{ $result['total_payment'] }}" readonly @endif required/>
+                                        </div>
                                     </div>
                                 </div>    
                                 <div class="form-group">
