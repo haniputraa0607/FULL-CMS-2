f1b20933bca5260b043d4cf576690c8e725063ab
tpid-hanif <hanif@technopartner.co.id>
2022-01-12 13:38:58 +0700

    updated view detail transaction shop

--- Modules/Transaction/Resources/views/transactionDetailShop.blade.php
+++ Modules/Transaction/Resources/views/transactionDetailShop.blade.php
@@ -530,7 +530,7 @@ $configs = session('configs');
 	                        <div class="col-6 text-13-3px WorkSans-Medium text-grey space-text">{{$dt['name']}}</div>
 	                        <div class="col-6 text-13-3px text-right WorkSans-Medium text-grey-red space-text">{{ str_replace(',', '.', $dt['amount']) }}</div>
                             @else
-	                        <div class="col-6 text-13-3px WorkSans-Medium text-grey-light space-text">{{$dt['name']}} ({{$dt['desc']}})</div>
+	                        <div class="col-6 text-13-3px WorkSans-Medium text-grey-light space-text">{{$dt['name']}}</div>
 	                        <div class="col-6 text-13-3px text-right WorkSans-Medium text-grey-light space-text">{{ str_replace(',', '.', $dt['amount']) }}</div>
 							@endif
 	                    @endif
