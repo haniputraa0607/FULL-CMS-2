57fdec0dec5d99c144d0b01b90ed8137c5e309a3
Toto Prasetyo <toto@technopartner.co.id>
2022-01-17 08:53:46 +0700

    be outlet bundling

---
+++ Modules/BusinessDevelopment/Http/Controllers/OutletStarterBundlingController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Modules\BusinessDevelopment\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Routing\Controller;
+use App\Lib\MyHelper;
+
+class OutletStarterBundlingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     * @return Response
+     */
+    public function index()
+    {
+        $data = [
+            'title'          => 'Outlet Starter Bundling',
+            'url_title'      => url()->current(),
+            'sub_title'      => 'Manage Outlet Starter Bundling',
+            'menu_active'    => 'outlet-starter-bundling',
+            'submenu_active' => 'outlet-starter-bundling-list',
+            'result'         => MyHelper::get('outlet-starter-bundling')['result'],
+        ];
+
+        return view('businessdevelopment::bundling.list', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return Response
+     */
+    public function create()
+    {
+        $data = [
+            'title'          => 'Outlet Starter Bundling',
+            'url_title'      => url()->current(),
+            'sub_title'      => 'Create Outlet Starter Bundling',
+            'menu_active'    => 'outlet-starter-bundling',
+            'submenu_active' => 'new-outlet-starter-bundling',
+        ];
+        return view('businessdevelopment::bundling.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $result = MyHelper::post('outlet-starter-bundling/create', $request->all());
+        if ($result['status'] == 'success') {
+            return redirect(url('outlet-starter-bundling/detail/' . $request->code))->withSuccess([$result['result']['message'] ?? 'Success']);
+        } else {
+            return back()->withErrors($result['messages'] ?? ['Something went wrong'])->withInput();
+        }
+    }
+
+    /**
+     * Show the specified resource.
+     * @param int $idICOUNT_URL = http://dev.ima.icount.id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $data = [
+            'title'          => 'Outlet Starter Bundling',
+            'url_title'      => url()->current(),
+            'sub_title'      => 'Create Outlet Starter Bundling',
+            'menu_active'    => 'outlet-starter-bundling',
+            'submenu_active' => 'outlet-starter-bundling-list',
+            'bundling'       => MyHelper::post('outlet-starter-bundling/detail', ['code' => $id])['result'] ?? false,
+            'product_icounts' => MyHelper::post('outlet-starter-bundling/icount-product', ['code' => $id])['result'] ?? [],
+        ];
+
+        if (!$data['bundling']) {
+            abort(404);
+        }
+        return view('businessdevelopment::bundling.show', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        $result = MyHelper::post('outlet-starter-bundling/update', $request->all());
+        if ($result['status'] == 'success') {
+            return redirect(url('outlet-starter-bundling/detail/' . $request->code))->withSuccess([$result['result']['message'] ?? 'Success']);
+        } else {
+            return redirect(url('outlet-starter-bundling/detail/' . $request->code))->withErrors($result['messages'] ?? ['Something went wrong'])->withInput();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param int $id
+     * @return Response
+     */
+    public function destroy(Request $request)
+    {
+        $result = MyHelper::post('outlet-starter-bundling/delete', $request->all());
+        if ($result['status'] == 'success') {
+            return redirect(url('outlet-starter-bundling'))->withSuccess([$result['result']['message'] ?? 'Success']);
+        } else {
+            return redirect(url('outlet-starter-bundling'))->withErrors($result['messages'] ?? ['Something went wrong'])->withInput();
+        }
+    }
+}
---
+++ Modules/BusinessDevelopment/Resources/views/bundling/list.blade.php
@@ -0,0 +1,92 @@
+<?php
+    use App\Lib\MyHelper;
+    $grantedFeature     = session('granted_features');
+ ?>
+@extends('layouts.main')
+
+@section('page-style')
+@endsection
+
+@section('page-plugin')
+<script src="{{ env('STORAGE_URL_VIEW') }}{{('assets/global/plugins/bootstrap-confirmation/bootstrap-confirmation.min.js') }}" type="text/javascript"></script>
+@endsection
+
+@section('page-script')
+<script type="text/javascript">
+    function deleteBundling(id) {
+        alert(id);
+    }
+</script>
+@endsection
+
+@section('content')
+    <div class="page-bar">
+        <ul class="page-breadcrumb">
+            <li>
+                <a href="/">Home</a>
+                <i class="fa fa-circle"></i>
+            </li>
+            <li>
+                <span>{{ $title }}</span>
+                @if (!empty($sub_title))
+                    <i class="fa fa-circle"></i>
+                @endif
+            </li>
+            @if (!empty($sub_title))
+            <li>
+                <span>{{ $sub_title }}</span>
+            </li>
+            @endif
+        </ul>
+    </div>
+    <br>
+    @include('layouts.notifications')  
+
+    <div class="portlet light bordered">
+        <div class="portlet-title">
+            <div class="caption">
+                <span class="caption-subject font-blue sbold uppercase">Outlet Starter Bundling</span>
+            </div>
+        </div>
+        <div class="portlet-body form">
+            <br>
+            <div style="white-space: nowrap;">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered table-hover" id="kt_datatable">
+                        <thead>
+                        <tr>
+                            <th class="text-nowrap text-center">Name</th>
+                            <th class="text-nowrap text-center">Code</th>
+                            <th class="text-nowrap text-center">Description</th>
+                            <th class="text-nowrap text-center">Status</th>
+                            <th class="text-nowrap text-center">Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @if(!empty($result['data']))
+                            @foreach($result['data'] as $dt)
+                                <tr>
+                                    <td>{{$dt['name']}}</td>
+                                    <td>{{$dt['code']}}</td>
+                                    <td>{{$dt['description']}}</td>
+                                    <td>{{$dt['status'] ? 'Active' : 'Inactive'}}</td>
+                                    <td>
+                                        <a href="{{ url('outlet-starter-bundling/detail/'.$dt['code']) }}" class="btn btn-sm blue text-nowrap"><i class="fa fa-pencil"></i> Edit</a>
+                                        <button type="button" class="btn btn-sm red sweetalert-delete btn-primary" onclick="deleteBundling({{ $dt['id_outlet_starter_bundling'] }})" data-name="{{ $dt['name'] }}" data-toggle="confirmation"><i class="fa fa-trash-o"></i> Delete</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td colspan="5" style="text-align: center">Data Not Available</td>
+                            </tr>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    </form>
+    <br>
+@endsection
