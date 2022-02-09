4650bee7cc2bb5b2e6dd57e8877ddb41ebbaed60
raka yulian <89185735+tpid-raka@users.noreply.github.com>
2022-02-09 07:15:18 +0700

    CUT OFF

--- Modules/Setting/Http/Controllers/SettingController.php
+++ Modules/Setting/Http/Controllers/SettingController.php
@@ -1624,7 +1624,7 @@ class SettingController extends Controller
             'submenu_active'    => 'setting-attendances-date',
         ];
         if($post){
-            $query = MyHelper::post('setting/attendances_date_create', $post);
+            $query = MyHelper::post('setting/attendances_date_create', $post);;
             if(($query['status']??'')=='success'){
                 return redirect('setting/setting-attendances-date')->with('success',['Success update data']);
             }else{
@@ -1633,7 +1633,8 @@ class SettingController extends Controller
         }else{
             $query = MyHelper::get('setting/attendances_date');
             $data['result'] = $query;
-            return $data;
+            $data['insentif'] = MyHelper::get('recruitment/hairstylist/be/group/setting_insentif')['result']??null;
+            $data['cuts_salary'] = MyHelper::get('recruitment/hairstylist/be/group/setting_potongan')['result']??null;
             return view('setting::attendances_date', $data);
         }
     }
