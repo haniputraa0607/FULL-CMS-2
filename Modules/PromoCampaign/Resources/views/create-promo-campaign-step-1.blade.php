0fd810dfa77d3ae532b0126e5d280908da5bb2af
Dina <dina@technopartner.co.id>
2022-03-15 10:00:46 +0700

    change wrong name promo description

--- Modules/PromoCampaign/Resources/views/create-promo-campaign-step-1.blade.php
+++ Modules/PromoCampaign/Resources/views/create-promo-campaign-step-1.blade.php
@@ -448,7 +448,7 @@
 						<span class="required" aria-required="true"> * </span>
 						<i class="fa fa-question-circle tooltips" data-original-title="Deskripsi Promo" data-container="body"></i>
 						<div class="input-group col-md-12">
-							<textarea required type="text" class="form-control" name="promo_campaign_description" placeholder="Promo Description">{{$result['promo_campaign_description']??old('promo_campaign_description')}}</textarea>
+							<textarea required type="text" class="form-control" name="promo_description" placeholder="Promo Description">{{$result['promo_description']??old('promo_description')}}</textarea>
 						</div>
 					</div>
 					<div class="form-group">
