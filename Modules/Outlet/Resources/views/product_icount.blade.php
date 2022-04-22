5521f585763a375433e59b7522e5e355ccca75b0
Toto Prasetyo <toto@technopartner.co.id>
2022-04-22 10:50:46 +0700

    stock adjustment

--- Modules/Outlet/Resources/views/product_icount.blade.php
+++ Modules/Outlet/Resources/views/product_icount.blade.php
@@ -1,4 +1,7 @@
 @php $out = $outlet[0]; @endphp
+<div style="padding:10px 0" class="text-right">
+    <a href="#modal_stock_adjustment" data-toggle="modal" class="btn btn-primary">Stock Adjustment</a>
+</div>
 <table class="table datatable">
     <thead>
         <tr>
@@ -25,3 +28,71 @@
         @endforeach
     </tbody>
 </table>
+<div class="modal fade bd-example-modal-sm" id="modal_stock_adjustment" role="dialog" aria-labelledby="stockAdjustmentModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Stock Adjustment</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body" style="padding: 15px !important">
+                <form role="form" action="{{ url('outlet/detail') }}/{{ $outlet[0]['outlet_code'] }}/stock-adjustment" id="form_stock_adjustment_submit" method="post" enctype="multipart/form-data">
+                    <div class="form-body">
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Title</label>
+                            <div class="col-md-8">
+                                <input name="title" class="form-control" placeholder="Write stock adjustment title" value="Stock Adjustment">
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Product Icount</label>
+                            <div class="col-md-8">
+                                <select class="select_product_icount" name="id_product_icount" data-placeholder="Select product icount" onchange="updateSelectUnit()"></select>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Unit</label>
+                            <div class="col-md-4">
+                                <select class="select2" name="unit" data-placeholder="Select unit"></select>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Current Stock</label>
+                            <div class="col-md-3">
+                                <input type="number" class="form-control" name="current_stock" disabled>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">New Stock</label>
+                            <div class="col-md-3">
+                                <input type="number" class="form-control" name="new_stock" onchange="adjustStock('new_stock')" required>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Stock Adjustment</label>
+                            <div class="col-md-3">
+                                <input type="number" class="form-control" name="stock_adjustment" onchange="adjustStock('stock_adjustment')" required>
+                            </div>
+                        </div>
+                        <div class="row" style="margin-bottom:10px">
+                            <label class="col-md-3 text-right" style="padding-top:5px">Notes</label>
+                            <div class="col-md-8">
+                                <textarea name="notes" class="form-control" placeholder="Write notes here"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                </form>
+                <div class= "output">
+                    
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick="submitStockAdjustment()">Submit</button>
+            </div>
+        </div>
+    </div>
+</div>
