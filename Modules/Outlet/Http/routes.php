85935352875473c3a57900e7cb8d45a1b18a09c2
Toto Prasetyo <toto@technopartner.co.id>
2022-05-25 14:23:57 +0700

    fix features update schedule

--- Modules/Outlet/Http/routes.php
+++ Modules/Outlet/Http/routes.php
@@ -60,7 +60,7 @@ Route::group(['middleware' => ['web', 'validate_session'], 'prefix' => 'outlet',
 
     // photo
     Route::post('photo/delete', ['middleware' => 'feature_control:31', 'uses' => 'OutletController@deletePhoto']);
-    Route::post('schedule/save', ['middleware' => 'feature_control:31', 'uses' => 'OutletController@scheduleSave']);
+    Route::post('schedule/save', ['middleware' => [], 'uses' => 'OutletController@scheduleSave']);
     //different price
     Route::get('different-price', ['middleware' => 'feature_control:24', 'uses' => 'OutletController@differentPrice']);
     Route::post('different-price', ['middleware' => 'feature_control:24', 'uses' => 'OutletController@getDifferentPrice']);
